13da17f450a86c0173c096c94086105118f4097c
unknown <sarvanmaurya037@gmail.com>
2026-05-04 19:53:30 +0530

    Even Odd

--- basic/evenodd.java
+++ basic/evenodd.java
@@ -1,5 +1,13 @@
 package basic;
 
 public class evenodd {
-    
+    public static void main(String[] args) {
+        int num=30;
+
+        if(num%2==0){
+            System.out.println("Number Is Even " + num);
+        }else{
+            System.out.println("Number is odd " + num);
+        }
+    }
 }
