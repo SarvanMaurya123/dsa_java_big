11025e0364d3f6ccf26ed44f33c7910353dea25a
unknown <sarvanmaurya037@gmail.com>
2026-05-05 23:33:37 +0530

    array basic

---
+++ array/arrayunderstand.java
@@ -0,0 +1,20 @@
+package array;
+
+public class arrayunderstand {
+
+    public static void main(String[] args) {
+        // array is a collection of  data for same type;
+        // don't Mix data like string ,int, and float;
+        // only single type data in collect the array
+
+        // define this type
+        // int[]; here is data type
+        // roll; refrence varible
+        // new int[5]; create a object in heap memory also Dynamic memory allocation
+       int[] roll= new int[5]; 
+
+       
+       //define this type also
+       int[] roll2= {23,45,4,54,65};
+    }
+}
---
+++ basic/sumoffnatural.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class sumoffnatural {
+    public static void main(String[] args) {
+        int num=10;
+        int add=0;
+        for(int i=0; i<=num; i++){
+           add = add+i;
+        }
+        System.out.println("Sum of Natural Number:"+add);
+    }
+}
